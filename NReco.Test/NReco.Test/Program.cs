ee66b13998f6e57e25b49a517042b65adc89cc63
Tamara <tamara.fellner@gmx.net>
2020-11-26 17:51:39 +0100

    Added command UADD, MADD, UCLEAR, MCLEAR, C and updated command E and P

--- NReco.Test/NReco.Test/Program.cs
+++ NReco.Test/NReco.Test/Program.cs
@@ -44,6 +44,26 @@ namespace NReco.Test
                 }
             }
 
+            // load users
+            List<int> users = new List<int>();
+            using (TextReader reader = File.OpenText(@"data/ratings.csv"))
+            {
+                CsvReader csv = new CsvReader(reader);
+                csv.Configuration.Delimiter = ",";
+                csv.Configuration.MissingFieldFound = null;
+                while (csv.Read())
+                {
+                    if (int.TryParse(csv.GetField(0), out int id))
+                    {
+                        if (!users.Contains(id))
+                        {
+                            users.Add(id);
+                        }
+                    }
+                }
+            }
+
+
             // load data model
             var model = new FileDataModel("data/ratings.csv", false, FileDataModel.DEFAULT_MIN_RELOAD_INTERVAL_MS, false);
 
@@ -52,9 +72,9 @@ namespace NReco.Test
             string similarityType = "Cosine Similarity";
             string recommenderType = "Userbased";
             int neighborhoodSize = 125;
-            int userId = 72;
             int resultCount = 10;
-            int predictionMovieId = 1374;
+            List<int> movieIdPredList = new List<int>();
+            List<int> userIdPredList = new List<int>();
 
             //default recommender
             AbstractSimilarity similarity = new UncenteredCosineSimilarity(model);
@@ -129,22 +149,112 @@ namespace NReco.Test
                 }
                 else if (input == "C")
                 {
-                    //Todo: Konfiguration??? neu erstellen und Datendatei neu lesen
+                    Console.WriteLine("Wie groß soll die Nachbarschaft sein?");
+                    string neighborhoodSizeInput = Console.ReadLine();
+                    if (int.TryParse(neighborhoodSizeInput, out int neighborhoodSizeParsed))
+                    {
+                        neighborhoodSize = neighborhoodSizeParsed;
+                        neighborhood = new NearestNUserNeighborhood(neighborhoodSize, similarity, model);
+                        Console.WriteLine("Nachbarschaftsgröße auf " + neighborhoodSize + " geändert.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error: Es wurde keine Zahl eingegeben.");
+                        Console.WriteLine("Die Nachbarschaftsgröße bleibt bei " + neighborhoodSize + ".");
+                    }
+
+                    Console.WriteLine("Wieviele Filme sollen vorgeschlagen werden?");
+
+                    string resultCountInput = Console.ReadLine();
+                    if (int.TryParse(resultCountInput, out int resultCountParsed))
+                    {
+                        resultCount = resultCountParsed;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error: Es wurde keine Zahl eingegeben.");
+                        Console.WriteLine("Die Anzahl der vorgeschlagenen Filme bleibt bei " + resultCount + ".");
+                    }
+
+                    foreach (int userId in userIdPredList)
+                    {
+                        Console.WriteLine("Vorgeschlagene Filme für User mit UserId " + userId);
+                        IList<IRecommendedItem> recommendedItems = recommender.Recommend(userId, resultCount);
+
+                        foreach (var recItem in recommendedItems)
+                        {
+                            Console.WriteLine("Item: " + recItem.GetItemID() + " (" + movies.First(x => x.movieId == recItem.GetItemID()).title + ") ===> " + recItem.GetValue());
+                        }
+                        Console.WriteLine();
+
+                    }
+
+                    // load movies
+                    movies = new List<MovieRecord>();
+                    using (TextReader reader = File.OpenText(@"data/movies.csv"))
+                    {
+                        CsvReader csv = new CsvReader(reader);
+                        csv.Configuration.Delimiter = ",";
+                        csv.Configuration.MissingFieldFound = null;
+                        while (csv.Read())
+                        {
+                            movies.Add(csv.GetRecord<MovieRecord>());
+                        }
+                    }
+
+                    // load users
+                    users = new List<int>();
+                    using (TextReader reader = File.OpenText(@"data/ratings.csv"))
+                    {
+                        CsvReader csv = new CsvReader(reader);
+                        csv.Configuration.Delimiter = ",";
+                        csv.Configuration.MissingFieldFound = null;
+                        while (csv.Read())
+                        {
+                            if (int.TryParse(csv.GetField(0), out int id))
+                            {
+                                if (!users.Contains(id))
+                                {
+                                    users.Add(id);
+                                }
+                            }
+                        }
+                    }
+
+                    model = new FileDataModel("data/ratings.csv", false, FileDataModel.DEFAULT_MIN_RELOAD_INTERVAL_MS, false);
+
+                    if(similarityType == "Cosine Similarity")
+                    {
+                        similarity = new UncenteredCosineSimilarity(model);
+                    }
+                    else
+                    {
+                        similarity = new PearsonCorrelationSimilarity(model);
+                    }
+
+                    neighborhood = new NearestNUserNeighborhood(neighborhoodSize, similarity, model);
+
+                    if(recommenderType == "Itembased")
+                    {
+                        recommender = new GenericItemBasedRecommender(model, similarity);
+                    }
+                    else
+                    {
+                        recommender = new GenericUserBasedRecommender(model, neighborhood, similarity);
+                    }
                 }
                 else if (input == "O")
                 {
-                    int countUser = 0; //Todo: Anzahl User zählen
-                    int countMovies = movies.Count;
+                    int countPredUser = userIdPredList.Count;
+                    int countPredMovies = movieIdPredList.Count;
 
                     Console.WriteLine("-----------------------------------------------");
                     Console.WriteLine("Recommender Typ: " + recommenderType);
                     Console.WriteLine("Similarity Typ: " + similarityType);
                     Console.WriteLine("Nachbarschaftsgröße: " + neighborhoodSize);
                     Console.WriteLine("Anzahl Ergebnisse: " + resultCount);
-                    Console.WriteLine("User Id: " + userId);
-                    Console.WriteLine("Prediction Movie Id: " + predictionMovieId);
-                    Console.WriteLine("Anzahl Benutzer: " + countUser);
-                    Console.WriteLine("Anzahl Filme: " + countMovies);
+                    Console.WriteLine("Anzahl Benutzer: " + countPredUser);
+                    Console.WriteLine("Anzahl vorgeschlagene Filme: " + countPredMovies);
                     Console.WriteLine("-----------------------------------------------");
                 }
                 else if (input == "E")
@@ -161,71 +271,96 @@ namespace NReco.Test
                         Console.WriteLine("Error: Es wurde keine Zahl eingegeben.");
                         Console.WriteLine("Die Anzahl der vorgeschlagenen Filme bleibt bei " + resultCount + ".");
                     }
-                    Console.WriteLine("Vorgeschlagene Filme:");
-                    IList<IRecommendedItem> recommendedItems = recommender.Recommend(userId, resultCount);
 
-                    foreach (var recItem in recommendedItems)
+                    foreach(int userId in userIdPredList)
                     {
-                        Console.WriteLine("Item: " + recItem.GetItemID() + " (" + movies.First(x => x.movieId == recItem.GetItemID()).title + ") ===> " + recItem.GetValue());
+                        Console.WriteLine("Vorgeschlagene Filme für User mit UserId " + userId);
+                        IList<IRecommendedItem> recommendedItems = recommender.Recommend(userId, resultCount);
+
+                        foreach (var recItem in recommendedItems)
+                        {
+                            Console.WriteLine("Item: " + recItem.GetItemID() + " (" + movies.First(x => x.movieId == recItem.GetItemID()).title + ") ===> " + recItem.GetValue());
+                        }
+                        Console.WriteLine();
+
                     }
+
+
 
                 }
                 else if (input == "P")
                 {
-                    Console.WriteLine("Für welche Movie Id soll eine Prediction ausgegeben werden?");
-                    string predictionMovieIdInput = Console.ReadLine();
-                    if (int.TryParse(predictionMovieIdInput, out int predictionMovieIdParsed))
+                    foreach(int userId in userIdPredList)
+                    {
+                        Console.WriteLine("Prediction für User mit Id" + userId + ":");
+                        foreach(int movieId in movieIdPredList)
+                        {
+                            float predictionValue = recommender.EstimatePreference(userId, movieId);
+                            Console.WriteLine(movieId + " -> " + movies.First(x => x.movieId == movieId).title + " ===> " + predictionValue);
+                        }
+                        Console.WriteLine();
+                    }
+                }
+                else if (input == "UADD")
+                {
+                    Console.WriteLine("Welcher Benutzer (Id) soll zur Liste für Vorschläge hinzugefügt werden?");
+
+                    string userIdInput = Console.ReadLine();
+                    if (int.TryParse(userIdInput, out int userIdParsed))
+                    {
+                        if (users.Contains(userIdParsed))
+                        {
+                        userIdPredList.Add(userIdParsed);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Error: UserId existiert nicht.");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error: Es wurde keine Zahl eingegeben.");
+                    }
+                }
+                else if (input == "MADD")
+                {
+                    Console.WriteLine("Welcher Film (Id) soll zur Liste für Vorschläge hinzugefügt werden?");
+                    string movieIdInput = Console.ReadLine();
+                    if (int.TryParse(movieIdInput, out int movieIdParsed))
                     {
                         bool movieIdFound = false;
                         foreach (MovieRecord movie in movies)
                         {
-                            if (movie.movieId == predictionMovieIdParsed)
+                            if (movie.movieId == movieIdParsed)
                             {
-                                predictionMovieId = predictionMovieIdParsed;
+                                movieIdPredList.Add(movieIdParsed);
                                 movieIdFound = true;
                                 break;
                             }
                         }
                         if (!movieIdFound)
                         {
-                            Console.WriteLine("Error: movieId existiert nicht");
-                            Console.WriteLine("Es wird eine Prediction für die zuletzt eingegebene Movie Id " + predictionMovieId + " berechnet.");
+                            Console.WriteLine("Error: movieId existiert nicht. Es wurde kein Film zur Liste hinzugefügt");
                         }
                     }
                     else
                     {
                         Console.WriteLine("Error: Es wurde keine Zahl eingegeben.");
-                        Console.WriteLine("Es wird eine Prediction für die zuletzt eingegebene Movie Id " + predictionMovieId + " berechnet.");
                     }
-
-                    var predictionValue = recommender.EstimatePreference(userId, predictionMovieId);
-                    Console.WriteLine("Prediction: " + predictionMovieId + " (" + movies.First(x => x.movieId == predictionMovieId).title + " ===> " + predictionValue);
-                }
-                else if (input == "UADD")
-                {
-                    //Todo: Benutzer zur Liste für Vorschläge hinzufügen
-                }
-                else if (input == "MADD")
-                {
-                    Console.WriteLine("Wie heißt der Film, der hinzugefügt werden soll?");
-                    string title = Console.ReadLine();
-                    int movieId = movies.Last().movieId++;
-                    movies.Add(new MovieRecord(movieId, title));
                 }
                 else if (input == "UCLEAR")
                 {
-                    //Todo: Benutzerliste leeren
+                    userIdPredList.Clear();
                 }
                 else if (input == "MCLEAR")
                 {
-                    movies.Clear();
+                    movieIdPredList.Clear();
                 }
             }
 
-            Console.WriteLine("Programm wird beendet");
+            Console.WriteLine("Programm beendet");
 
-            Console.ReadLine();
-            
+            Console.ReadLine();   
         }
     }
 }
